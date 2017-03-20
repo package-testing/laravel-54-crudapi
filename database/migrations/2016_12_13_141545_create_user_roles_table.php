2d73d1e23956b7706729e641b97862c29fd4117a
davidcraig <davidcraig1986@gmail.com>
2017-03-20 16:22:31 +0000

    Add migrations

---
+++ database/migrations/2016_12_13_141545_create_user_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRolesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')
+                  ->references('id')->on('roles')
+                  ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
